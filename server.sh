d9001ba7043053fa8e6a390d34e187895a2f691a
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-28 21:25:17 +0000

    remove password

--- server.sh
+++ server.sh
@@ -3,7 +3,6 @@ docker build -t sim . && \
 docker run --privileged -it --rm --gpus all \
   -p 40001:8081 \
   -e RESOLUTION=1920x1080 \
-  -e VNCPASS=pass \
   --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly \
   --mount type=bind,source=/var/run/dbus/system_bus_socket,target=/var/run/dbus/system_bus_socket \
   --name sim sim
