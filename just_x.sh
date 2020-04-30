43ba4f589f3e4e74d67b80e2c17556caeee85b60
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-30 20:18:07 +0000

    looks good

--- just_x.sh
+++ just_x.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-docker build -t justx . && \
 docker run --privileged --rm --gpus all \
   -e RESOLUTION=1920x1080 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   --mount type=bind,source=/var/run/dbus/system_bus_socket,target=/var/run/dbus/system_bus_socket \
-  --name justx justx
+  --name justx paulruvolo/testneato
--- turbovnc/start_rnw.sh
+++ turbovnc/start_rnw.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-docker build -t turbovnc . && \
+
+if [ "$#" -ne 1 ]; then
+    echo "./start_rnw.sh instance-id"
+    exit 1
+fi
+let "NO_VNC_PORT=40001 + $1"
+
 docker run --rm \
   --gpus all \
-  -p 40001:8081 \
+  -p $NO_VNC_PORT:8081 \
   -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw \
   --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly \
-  --name turbovnc -it turbovnc
+  -it paulruvolo/testneato:turbovnc
