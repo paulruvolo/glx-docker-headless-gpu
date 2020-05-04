a06a3e84286ebccab0df9513f1aafe13abf57ba5
Ubuntu <ubuntu@ip-172-31-44-97.ec2.internal>
2020-05-04 03:24:04 +0000

    bring dbus inside the justx container

--- just_x.sh
+++ just_x.sh
@@ -2,5 +2,4 @@
 docker run --privileged --rm --gpus all \
   -e RESOLUTION=1920x1080 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  --mount type=bind,source=/var/run/dbus/system_bus_socket,target=/var/run/dbus/system_bus_socket \
   --name justx paulruvolo/testneato
--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 # inside docker script
 trap 'kill $(jobs -p)' EXIT
 
+dbus-uuidgen > /var/lib/dbus/machine-id
+mkdir -p /var/run/dbus
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
 # 0. generate xorg.conf
 BUS_ID=$(nvidia-xconfig --query-gpu-info | grep 'PCI BusID' | sed -r 's/\s*PCI BusID : PCI:(.*)/\1/')
 nvidia-xconfig -a --virtual=$RESOLUTION --allow-empty-initial-configuration --enable-all-gpus --busid $BUS_ID
