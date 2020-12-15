049b2d0fa88bce8f48277a70abac51b26731070b
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-12-14 22:33:26 -0500

    added an adapter to nvidia driver 450

--- turbovnc/start_rnw.sh
+++ turbovnc/start_rnw.sh
@@ -6,9 +6,9 @@ if [ "$#" -ne 1 ]; then
 fi
 let "NO_VNC_PORT=40001 + $1"
 
-# If you use the containers in ECS, then MATLAB is already installed
+# remap :1 to :0 to avoid having to modify the container (could just use $DISPLAY to figure out the right x-server)
 docker run --rm \
   -d --gpus all \
   -p $NO_VNC_PORT:8081 \
-  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -it qeasimnvidia435
+  -v /tmp/.X11-unix/X1:/tmp/.X11-unix/X0:rw \
+  -it qeasim450
