879a3e5a153b566adca50eb16e8adbb4a931a850
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-12-15 00:10:33 -0500

    added smarter parsing of display environment variable

--- turbovnc/start_rnw.sh
+++ turbovnc/start_rnw.sh
@@ -6,9 +6,11 @@ if [ "$#" -ne 1 ]; then
 fi
 let "NO_VNC_PORT=40001 + $1"
 
-# remap :1 to :0 to avoid having to modify the container (could just use $DISPLAY to figure out the right x-server)
+DISPLAY_NUM=$(echo $DISPLAY | sed 's/^.//')
+
+# remap the active display (assumed to be the one for 3d rendering) to :0 to avoid having to modify the container
 docker run --rm \
   -d --gpus all \
   -p $NO_VNC_PORT:8081 \
-  -v /tmp/.X11-unix/X1:/tmp/.X11-unix/X0:rw \
+  -v /tmp/.X11-unix/X$DISPLAY_NUM:/tmp/.X11-unix/X0:rw \
   -it qeasim450
